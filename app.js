18e6bedd621457d18e19275fdb530950af0a5206
Anderson Fae <andersonfae.owl@gmail.com>
2022-07-12 16:21:05 -0300

    Create Done

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const userRouter = require("./routes/user.routes");
+app.use("/user", userRouter);
+
 app.listen(4000, () => {
   console.log("Servidor run 4000");
 });
